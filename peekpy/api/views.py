b1e2f46b32110bfbd1db2e834d1ea9ffe01037ed
Alexandre <meda.alexandre@gmail.com>
2015-07-28 01:33:08 +0200

    Add exception for the API

--- peekpy/api/views.py
+++ peekpy/api/views.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from flask.views import MethodView
 
+from .exception import APIException
 
 api = Blueprint('api', __name__, url_prefix='/api')
+
+
+#Register the api exception handler
+@api.errorhandler(APIException)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+class FolderAPI(MethodView):
+
+    def get(self):
+        return "<h1> GET method</h1>"
+
+    def post(self):
+        return "<h1> POST method</h1>"
+
+#Register the method view
+api.add_url_rule('/f/', view_func=FolderAPI.as_view('folder'))
